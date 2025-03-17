8e024351ce5e0a0c6e5f7b36cfc8f45bfb59de88
0xSardius <0xSardius@gmail.com>
2025-03-17 10:37:15 -0400

    player groups updated

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updateable  = pygame.sprite.Group() 
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
 
     while True:
         # use screen's fill method to fill screen with a solid black color
@@ -24,10 +30,11 @@ def main():
 
         # Update game state
         dt = clock.get_time() / 1000
-        player.update(dt)
+        updateable.update(dt)
 
         # Draw everything
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
 
         # Cap the frame rate
