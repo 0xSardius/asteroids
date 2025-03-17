86b462fc0d4dcede738be175619a4fbf65aad42f
0xSardius <0xSardius@gmail.com>
2025-03-16 21:04:06 -0400

    player shape updated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -9,13 +9,33 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         # use screen's fill method to fill screen with a solid black color
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0, 0, 0))
+
+        # Update game state
+        dt = clock.get_time() / 1000
+        player.update(dt)
+
+        # Draw everything
+        player.draw(screen)
         pygame.display.flip()
+
+        # Cap the frame rate
+        clock.tick(60)
+    
+    pygame.quit()
+        
+        
 
 
 if __name__ == "__main__":
